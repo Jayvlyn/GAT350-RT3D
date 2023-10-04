bb6f65628de127c785714e7cc477c84de3611a6c
Jacob Leonardo <leonardojacob257@gmail.com>
2023-10-03 19:20:02 -0600

    OpenGL testing

--- Source/Application/World01.cpp
+++ Source/Application/World01.cpp
@@ -18,11 +18,13 @@ namespace nc
     {
         m_time += dt;
 
-        m_angle += 90 * dt;
         m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_A) ? -dt : 0;
         m_position.x += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_D) ? dt : 0;
         m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_W) ? dt : 0;
         m_position.y += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_S) ? -dt : 0;
+
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_Q) ? dt * 30 : 0;
+        m_angle += ENGINE.GetSystem<InputSystem>()->GetKeyDown(SDL_SCANCODE_E) ? -dt * 30 : 0;
     }
 
     void World01::Draw(Renderer& renderer)
@@ -33,18 +35,23 @@ namespace nc
         // render
         glPushMatrix();
         glTranslatef(m_position.x, m_position.y, 0);
-        //glRotatef(m_angle, 1, 1, 1);
-        glScalef((sin(m_time * 5) + 1) * 0.5f, 1, 1);
 
-        glBegin(GL_TRIANGLES);
+        glRotatef(m_angle, 0, 0, 1);
+
+        glScalef((sin(m_time * 5) + 1) * 0.5f, (sin(m_time * 5) + 1) * 0.5f, (sin(m_time * 5) + 1) * 0.5f);
+
+        glBegin(GL_QUAD_STRIP);
 
         glColor3f(1, 0, 0);
+        glVertex2f(-0.5f, 0.5f);
+
+        glColor3f(1, 0, 0);
+        glVertex2f(0.5f, 0.5f);
+
+        glColor3f(0, 1, 1);
         glVertex2f(-0.5f, -0.5f);
 
-        glColor3f(0, 1, 0);
-        glVertex2f(0, 0.5f);
-
-        glColor3f(0, 0, 1);
+        glColor3f(0, 1, 1);
         glVertex2f(0.5f, -0.5f);
 
         glEnd();
